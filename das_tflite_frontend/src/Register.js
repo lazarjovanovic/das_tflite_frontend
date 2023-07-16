0b30d8b2bac231f9a35665b8d04f682bd7370158
lazarjovanovic <Lufthansa4!>
2023-07-17 00:00:55 +0200

    Registration firm added

--- das_tflite_frontend/src/Register.js
+++ das_tflite_frontend/src/Register.js
@@ -4,9 +4,7 @@
 
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Navigate } from "react-router-dom";
 import axios from 'axios';
-import Register from "./Register";
 
 import logo from './logo.svg';
 import './styles/App.css';
@@ -20,13 +18,14 @@ function LoadingSpinner() {
   );
 }
 
-function App() {
+function Register() {
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [gender, setGender] = useState("Male")
 
   const errors = {
-    invalid_login: "Invalid login"
+    invalid_registration: "Invalid registration"
   };
 
   const handleSubmit = async (event) => {
@@ -34,31 +33,31 @@ function App() {
     event.preventDefault();
     setIsLoading(true);
 
-    var { uname, pass } = document.forms[0];
+    var { rname, rsurname, rusername, rpassword } = document.forms[0];
 
-    const encoded_pwd = btoa(pass.value)
+    const encoded_pwd = btoa(rpassword.value)
 
     const requestOptions = {
-      username: uname.value, 
-      //password: encoded_pwd
-      password: pass.value
+        name: rname.value,
+        surname: rsurname.value,
+        username: rusername.value,
+        password: encoded_pwd
     };
 
-    const response = await axios.post('http://localhost:10000/login', requestOptions);
+    const response = await axios.post('http://localhost:10000/register', requestOptions);
     const userData = response.data
     setIsLoading(false);
 
     if (userData) {
       if (userData.user_id === null) {
-        setErrorMessages({ name: "invalid_login_label", message: errors.invalid_login });
+        setErrorMessages({ name: "invalid_register_label", message: errors.invalid_registration });
       } else {
         const user_role = userData.user_role
         setIsSubmitted(true);
         // TODO redirect to another page
-        //navigate('/Register');
       }
     } else {
-      setErrorMessages({ name: "invalid_login_label", message: errors.invalid_login });
+      setErrorMessages({ name: "invalid_register_label", message: errors.invalid_registration });
     }
   };
 
@@ -71,16 +70,33 @@ function App() {
   // JSX code for login form
   const renderForm = (
     <div className="form">
-      <div className="title">Sign In</div>
       <form onSubmit={handleSubmit}>
         <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
+          <label>Name </label>
+          <input type="text" name="name" required />
+        </div>
+        <div className="input-container">
+          <label>Surname </label>
+          <input type="text" name="surname" required />
+        </div>
+        <div className="input-container">
+          <label>Surname </label>
+          <input type="text" name="username" required />
         </div>
         <div className="input-container">
           <label>Password </label>
           <input type="password" name="pass" required />
-          {renderErrorMessage("invalid_login_label")}
+          {renderErrorMessage("invalid_register_label")}
+        </div>
+
+        <div class="radio-buttons">
+            <input 
+                type="radio" 
+                name="optradio"/><label>Male</label>
+        
+            <input 
+                type="radio" 
+                name="optradio"/><label>Female</label>
         </div>
         <div className="button-container">
           <input type="submit" disabled={isLoading}/>
@@ -90,13 +106,11 @@ function App() {
   );
 
   return (
-    <div className="app">
-      <div className="login-form">
-        {isLoading ? <LoadingSpinner /> : (isSubmitted ? <Register/> : renderForm)}
-
-      </div>
+    <div className="register">
+        <div className="title">Log In</div>
+        {isLoading ? <LoadingSpinner /> : (isSubmitted ? <div>User is successfully registered</div> : renderForm)}
     </div>
   );
 }
 
-export default App;
+export default Register;
